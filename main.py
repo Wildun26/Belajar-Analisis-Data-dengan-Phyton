d2e5238485ff255ad29828dafbe5363d8a0c0be1
Wildun26 <130342493+Wildun26@users.noreply.github.com>
2024-03-02 14:18:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,32 +12,37 @@ data = muat_data()
 kolom1, kolom2 = st.columns(2)
 
 with kolom1:
-    # Sewa Sepeda Menurut Musim
+    # Jumlah Berbagi Sepeda Menurut Musim
     mapping_musim = {2: "musim panas", 3: "musim gugur"}
-    data["label_musim"] = data["musim"].map(mapping_musim)
+    data["label_musim"] = data["season"].map(mapping_musim)
 
     jumlah_musim = data.groupby("label_musim")["cnt"].sum().reset_index()
     grafik_jumlah_musim = px.bar(jumlah_musim, x="label_musim",
-                              y="cnt", title="Sewa Sepeda Menurut Musim")
+                              y="cnt", title="Jumlah Berbagi Sepeda Menurut Musim")
     st.plotly_chart(grafik_jumlah_musim, use_container_width=True,
                     height=400, width=600)
 
 with kolom2:
-    # Sewa Sepeda Menurut Situasi Cuaca
-    jumlah_cuaca = data.groupby("cuaca")["cnt"].sum().reset_index()
-    grafik_jumlah_cuaca = px.bar(jumlah_cuaca, x="cuaca",
-                               y="cnt", title="Sewa Sepeda Menurut Situasi Cuaca")
+    # Jumlah Berbagi Sepeda Menurut Situasi Cuaca
+    jumlah_cuaca = data.groupby("weathersit")["cnt"].sum().reset_index()
+    grafik_jumlah_cuaca = px.bar(jumlah_cuaca, x="weathersit",
+                               y="cnt", title="Jumlah Berbagi Sepeda Menurut Situasi Cuaca")
     st.plotly_chart(grafik_jumlah_cuaca, use_container_width=True, height=400, width=800)
 
-# Sewa Sepeda per Jam
-jumlah_per_jam = data.groupby("jam")["cnt"].sum().reset_index()
+# Jumlah Berbagi Sepeda per Jam
+jumlah_per_jam = data.groupby("hr")["cnt"].sum().reset_index()
 grafik_jumlah_per_jam = px.line(
-    jumlah_per_jam, x="jam", y="cnt", title="Sewa Sepeda per Jam")
+    jumlah_per_jam, x="hr", y="cnt", title="Jumlah Berbagi Sepeda per Jam")
 st.plotly_chart(grafik_jumlah_per_jam, use_container_width=True,
                 height=400, width=600)
 
-# Suhu vs. Sewa Sepeda
-grafik_suhu = px.scatter(data, x="suhu", y="cnt",
-                            title="Suhu vs. Sewa Sepeda")
+# Kelembapan vs. Jumlah Berbagi Sepeda
+grafik_kelembapan = px.scatter(
+    data, x="hum", y="cnt", title="Kelembapan vs. Jumlah Berbagi Sepeda")
+st.plotly_chart(grafik_kelembapan)
+
+# Suhu vs. Jumlah Berbagi Sepeda
+grafik_suhu = px.scatter(data, x="temp", y="cnt",
+                            title="Suhu vs. Jumlah Berbagi Sepeda")
 st.plotly_chart(grafik_suhu, use_container_width=True,
                 height=400, width=800)
